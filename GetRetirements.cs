81f492c05f0a54165b0c557deea54e914ca79bfd
Leo Durrant <leodurrant@microsoft.com>
2026-02-16 17:22:16 +0000

    wip: testing API calls

--- GetRetirements.cs
+++ GetRetirements.cs
@@ -1,29 +1,47 @@
-using System;
-using Azure.Identity;
-using Azure.ResourceManager;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Retirebot.Models;
+using System;
 
 namespace Retirebot
 {
     public class GetRetirements
     {
         private readonly ILogger _logger;
+        private readonly ManagementClient _managementClient;
 
-        public GetRetirements(ILoggerFactory loggerFactory)
+        public GetRetirements(ILoggerFactory loggerFactory, ManagementClient client)
         {
             _logger = loggerFactory.CreateLogger<GetRetirements>();
+            _managementClient = client;
         }
 
         [Function("GetRetirements")]
-        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] TimerInfo myTimer)
         {
-            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            //// Use _armClient to run Resource Graph queries
+            //// Example: var response = await _armClient.GetTenantResourceGraphAsync(content);
+
+            //_logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             
-            if (myTimer.ScheduleStatus is not null)
+            //if (myTimer.ScheduleStatus is not null)
+            //{
+            //    _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            //}
+
+            var subs = await _managementClient.GetSubscriptionsAsync();
+            QueryResult<ARGRetirementData> data = await _managementClient.RunQueryAsync(subs[0], "advisorresources | where properties.extendedProperties.recommendationSubCategory == \"ServiceUpgradeAndRetirement\" | where tostring(properties.category) has \"HighAvailability\" | extend resourceId = tostring(properties.resourceMetadata.resourceId) | project id, subscriptionId, resourceGroup, location, resourceId, ServiceID = tostring(properties.recommendationTypeId)");
+
+            for (int i = 0; i < data.Length; i++)
             {
-                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+                var advisoryId = data.Data[i].Id;
+                var advisory = await _managementClient.GetAdvisoryAsync(advisoryId);
+
+                return new OkObjectResult(advisory);
             }
+
+            return new OkObjectResult(data);
         }
     }
 }
