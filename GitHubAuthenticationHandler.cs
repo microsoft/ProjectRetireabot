3e73017ee735d9a5006109e8b4f3fcb9bb8304e8
Leo Durrant <leodurrant@microsoft.com>
2026-02-17 16:07:09 +0000

    feat: octokit, and authenticated GitHub client via DI

---
+++ GitHubAuthenticationHandler.cs
@@ -0,0 +1,33 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+
+namespace Retirebot;
+
+public static class GitHubAuthenticationHandler
+{
+    public static string GetJWT(long appId, string privateKey)
+    {
+        var rsa = RSA.Create();
+        rsa.ImportFromPem(privateKey);
+
+        var signingCredentials = new SigningCredentials(
+            new RsaSecurityKey(rsa),
+            SecurityAlgorithms.RsaSha256);
+
+        var now = DateTimeOffset.UtcNow;
+        var claims = new List<System.Security.Claims.Claim>
+        {
+            new System.Security.Claims.Claim(JwtRegisteredClaimNames.Iat, now.ToUnixTimeSeconds().ToString()),
+            new System.Security.Claims.Claim(JwtRegisteredClaimNames.Exp, now.AddMinutes(9).ToUnixTimeSeconds().ToString()),
+            new System.Security.Claims.Claim("iss", appId.ToString())
+        };
+
+        var jwtHeader = new JwtHeader(signingCredentials);
+        var jwtPayload = new JwtPayload((IEnumerable<System.Security.Claims.Claim>)claims);
+        var jwt = new JwtSecurityToken(jwtHeader, jwtPayload);
+
+        return new JwtSecurityTokenHandler().WriteToken(jwt);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Azure.Identity;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Octokit;
 using Retirebot;
 
 var builder = FunctionsApplication.CreateBuilder(args);
@@ -23,4 +24,25 @@ builder.Services.AddTransient(sp =>
 builder.Services.AddHttpClient<ManagementClient>(c => c.BaseAddress = new Uri("https://management.azure.com/"))
     .AddHttpMessageHandler<AzureCredentialTokenHandler>();
 
+builder.Services.AddSingleton(sp =>
+{
+    var appId = long.Parse(Environment.GetEnvironmentVariable("GITHUB_APP_ID") ?? throw new InvalidOperationException("GITHUB_APP_ID not configured"));
+    var privateKeyPath = Path.Combine(Directory.GetCurrentDirectory(), Environment.GetEnvironmentVariable("GITHUB_PRIVATE_KEY_PATH") ?? "private-key.pem");
+    var privateKey = File.ReadAllText(privateKeyPath);
+
+    var appClient = new GitHubClient(new ProductHeaderValue("Retirebot"))
+    {
+        Credentials = new Credentials(GitHubAuthenticationHandler.GetJWT(appId, privateKey), AuthenticationType.Bearer)
+    };
+
+    // If you need installation token instead of JWT, you'd do:
+    // var installationId = long.Parse(Environment.GetEnvironmentVariable("GITHUB_INSTALLATION_ID"));
+    // var response = appClient.GitHubApps.CreateInstallationToken(installationId).Result;
+    // return new GitHubClient(new ProductHeaderValue("Retirebot"))
+    // {
+    //     Credentials = new Credentials(response.Token)
+    // };
+
+    return appClient;
+});
 builder.Build().Run();
