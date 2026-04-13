56592e645ef2bbaa58601d3aaade0bf8a991871c
Leo Durrant <leodurrant@microsoft.com>
2026-04-13 16:54:13 +0100

    chore: removed unneeded security key

--- Helpers/EmptySecurityKey.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.IdentityModel.Tokens;
-
-namespace Retirebot.Helpers
-{
-    public class EmptySecurityKey : SecurityKey
-    {
-        public override int KeySize => 256;
-    }
-}
