cd442bb9a2d6bc2df4011d1e0e68af87a16200dd
Leo Durrant <leodurrant@microsoft.com>
2026-04-16 17:24:32 +0100

    feat: added test for skipping adding work items for existing work items based advisories

--- Retirebot.Tests/Functions/GetRetirementsTest.cs
+++ Retirebot.Tests/Functions/GetRetirementsTest.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Moq.Protected;
+using Octokit;
 using Retirebot.Functions;
 using Retirebot.Helpers;
 using Retirebot.Models;
@@ -117,6 +118,66 @@ namespace Retirebot.Tests.Functions
             mockWorkItemClient.Verify(
                 c => c.CreateBatchAsync(It.Is<List<Advisory>>(a => a.Count == 1), "example/repo", false),
                 Times.Once);
+        }
+
+        [Fact]
+        public async Task GetRetirementsAsync_ExistingIssues_SkipsCreation()
+        {
+            var config = BuildConfig(new Dictionary<string, string?>
+            {
+                [ConfigKeys.App.TargetRepository] = "example/repo",
+                [ConfigKeys.App.WorkItemScope] = WorkItemScope.Monolithic.ToString(),
+                [ConfigKeys.App.CreateChildWorkItems] = "true",
+            });
+
+            var (mgmtClient, handler) = BuildMockManagementClient();
+
+            handler.Protected()
+               .SetupSequence<Task<HttpResponseMessage>>("SendAsync",
+               ItExpr.IsAny<HttpRequestMessage>(),
+               ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(new HttpResponseMessage
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent(
+                    JsonSerializer.Serialize(new { value = new[] { new { subscriptionId = "sub-1" } } }),
+                    System.Text.Encoding.UTF8, "application/json"
+                   )
+               })
+               .ReturnsAsync(new HttpResponseMessage
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent(
+                        JsonSerializer.Serialize(TestData.CreateRetirementQueryResponse(1)),
+                        System.Text.Encoding.UTF8, "application/json")
+               });
+
+            var mockWorkItemClient = new Mock<IWorkItemClient>();
+
+            mockWorkItemClient
+                .Setup(c => c.FindExistingByAdvisoryAsync(It.IsAny<List<Advisory>>(), "example/repo"))
+                .ReturnsAsync(new Dictionary<string, WorkItem>
+                {
+                    ["rec-1"] = new WorkItem { Id = "1", Number = 1, Title = "Existing issue" }
+                });
+
+            mockWorkItemClient
+                .Setup(c => c.CreateBatchAsync(It.IsAny<List<Advisory>>(), "example/repo", false))
+                .ReturnsAsync(new List<(Advisory, WorkItem)>());
+
+            var loggerFactory = LoggerFactory.Create(b => b.AddDebug());
+            var sut = new GetRetirements(loggerFactory, config, mgmtClient, mockWorkItemClient.Object);
+
+            await sut.GetRetirementsASync();
+
+            mockWorkItemClient.Verify(
+                c => c.CreateBatchAsync(
+                    It.Is<List<Advisory>>(a => a.Count == 0),
+                    "example/repo",
+                    false
+                ),
+                Times.Once
+            );
         }
     }
 }
