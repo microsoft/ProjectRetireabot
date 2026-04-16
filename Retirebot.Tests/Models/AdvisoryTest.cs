d468caa2e04ee5e7cdbd461aa60788359e4ee794
Leo Durrant <leodurrant@microsoft.com>
2026-04-16 16:09:37 +0100

    feat: added tests for Advisory model

---
+++ Retirebot.Tests/Models/AdvisoryTest.cs
@@ -0,0 +1,48 @@
+using Retirebot.Models.Azure;
+
+namespace Retirebot.Tests.Models
+{
+    public class AdvisoryTest
+    {
+        [Fact]
+        public void GetSubscriptionId_ParsesFromId()
+        {
+            var advisory = new Advisory
+            {
+                Id = "/subscriptions/sub-123/resourceGroups/rg-1/providers/Microsoft.Advisor/recommendations/rec-1"
+            };
+
+            Assert.Equal("sub-123", advisory.GetSubscriptionId());
+        }
+
+        [Fact]
+        public void GetResourceGroupName_ParsesFromId()
+        {
+            var advisory = new Advisory
+            {
+                Id = "/subscriptions/sub-123/resourceGroups/my-rg/providers/Microsoft.Advisor/recommendations/rec-1"
+            };
+
+            Assert.Equal("my-rg", advisory.GetResourceGroupName());
+        }
+
+        [Fact]
+        public void GetResourceName_ReturnsImpactedValue()
+        {
+            var advisory = new Advisory
+            {
+                Properties = new AdvisoryProperties { ImpactedValue = "myapp" }
+            };
+
+            Assert.Equal("myapp", advisory.GetResourceName());
+        }
+
+        [Fact]
+        public void GetSubscriptionId_MalformedId_ThrowsIndexOutOfRange()
+        {
+            var advisory = new Advisory { Id = "/subscriptions" };
+
+            Assert.Throws<IndexOutOfRangeException>(() => advisory.GetSubscriptionId());
+        }
+    }
+}
