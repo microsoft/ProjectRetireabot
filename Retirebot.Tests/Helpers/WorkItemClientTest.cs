32d70e6f4582410ce4b657d5eabba7b99d90fb51
Leo Durrant <leodurrant@microsoft.com>
2026-04-16 14:47:33 +0100

    feat: structured test folders to match program

---
+++ Retirebot.Tests/Helpers/WorkItemClientTest.cs
@@ -0,0 +1,33 @@
+using Moq;
+using Retirebot.Helpers;
+using Retirebot.Models.Azure;
+using Xunit.Sdk;
+
+namespace Retirebot.Tests
+{
+    public class WorkItemClientTest
+    {
+        [Fact]
+        public async Task WorkItemClient_MockExample()
+        {
+            var mockClient = new Mock<IWorkItemClient>();
+
+            mockClient
+                .Setup(c => c.FindExistingByAdvisoryAsync(It.IsAny<List<Advisory>>(), It.IsAny<string>()))
+                .ReturnsAsync(new Dictionary<string, Models.WorkItem>());
+
+            mockClient
+                .Setup(c => c.CreateBatchAsync(It.IsAny<List<Advisory>>(), It.IsAny<string>(), It.IsAny<bool>()))
+                .ReturnsAsync(new List<(Advisory, Models.WorkItem)>());
+
+            IWorkItemClient client = mockClient.Object;
+            var result = await client.FindExistingByAdvisoryAsync([], "owner/repo");
+
+            Assert.Empty(result);
+
+            // Verify a method was (or wasn't) called
+            mockClient.Verify(c => c.FindExistingByAdvisoryAsync(It.IsAny<List<Advisory>>(), "owner/repo"), Times.Once);
+            mockClient.Verify(c => c.CreateBatchAsync(It.IsAny<List<Advisory>>(), It.IsAny<string>(), It.IsAny<bool>()), Times.Never);
+        }
+    }
+}
