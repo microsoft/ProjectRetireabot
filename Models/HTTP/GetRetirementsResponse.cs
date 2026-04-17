db6d6fae5404832e5694a15a3cbe024446feaa81
Leo Durrant <leodurrant@microsoft.com>
2026-04-17 16:07:18 +0100

    feat: added and updated response to add section for updated parent tracking work items

--- Models/HTTP/GetRetirementsResponse.cs
+++ Models/HTTP/GetRetirementsResponse.cs
@@ -1,7 +1,9 @@
+using System.Text.Json.Serialization;
 using Retirebot.Models.Azure;
 
-namespace Retirebot.Models
+namespace Retirebot.Models.HTTP
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum GetRetirementsResult
     {
         Unknown,
@@ -16,6 +18,7 @@ namespace Retirebot.Models
         public List<Advisory>? Advisories { get; set; }
         public List<WorkItem>? ExistingWorkItems { get; set; }
         public List<WorkItem>? NewWorkItems { get; set; }
+        public List<ParentWorkItemResult>? ParentWorkItems { get; set; }
         public double TimeElapsed { get; set; }
         public bool WhatIf { get; set; }
     }
